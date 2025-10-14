8022d044803a073d1f381b98c804dd62c2e1d1e2
umidjonvokhidov <umidjonvokhidov@proton.me>
2025-10-14 12:05:02 +0500

    implemented top-banner

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -1,7 +1,14 @@
+import ArrowRight from '@/public/icons/arrow-right.svg';
+
 export default function TopBanner() {
   return (
-    <div>
-      <h3>Top Banner</h3>
+    <div className="flex items-center justify-cente px-6 py-4 md:px-8 md:py-5 lg:px-10 lg:py-6">
+      <div className="flex items-center justify-between px-6 py-4 w-full rounded-sm bg-white/4 border border-white/8">
+        <h1 className="body-text-small-medium w-full">
+          Our startup program is live — enjoy 50% off today!
+        </h1>
+        <ArrowRight className="text-white size-5" />
+      </div>
     </div>
   );
 }
