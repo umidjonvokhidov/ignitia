a806509e8675811bcd0a0d4c940088f7f708cd01
umidjonvokhidov <umidjonvokhidov@proton.me>
2025-10-15 17:22:29 +0500

    implemented features section: 50% done

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,7 +1,124 @@
+'use client';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { FEATURES_ACCORDION } from '@/lib/constants';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Checkbox } from '@/components/ui/checkbox';
+import CustomButton from '@/components/CustomButton';
+import Facebook from '@/public/icons/Facebook.svg';
+import X from '@/public/icons/X.svg';
+import Google from '@/public/icons/Google.svg';
+
+const formSchema = z.object({
+  email: z.string().min(2).max(50),
+  remember: z.boolean(),
+});
+
 export default function Features() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      remember: false,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <div>
-      <h3>Features</h3>
-    </div>
+    <section className="flex flex-col w-full">
+      <div className="flex flex-col items-center px-4 py-6 gap-y-4 md:py-8 lg:py-10 border-b border-white/8">
+        <h3 className="text-h6 md:text-h4 lg:text-h3 inline-block w-full max-w-[448px] text-center">
+          Powerful features for <span className="handwrite">secure</span> access
+        </h3>
+        <p className="body-text-small-regular md:body-text-medium-regular text-center">
+          Provides all the tools you need to protect your users and data.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <Accordion type="multiple">
+          {FEATURES_ACCORDION.map(({ description, title }: FeatureItem) => (
+            <AccordionItem value={title} key={title} className="border-white/8">
+              <AccordionTrigger className="h-20 md:h-24 !body-text-large-semi-bold flex items-center py-4 px-3 md:px-10 md:py-8">
+                {title}
+              </AccordionTrigger>
+              <AccordionContent className="body-text-small-regular text-neutral-600 pb-4 px-3 md:px-10 md:pb-8">
+                {description}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+        <div className="p-4 md:p-10 w-full flex flex-col justify-center bg-white/4 relative group">
+          <div className="bg-[radial-gradient(68.33%_68.33%_at_50%_53.12%,_rgba(21,22,22,0)_0%,_#000000a0_100%)] w-full h-full absolute top-0 left-0 z-2" />
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-8 flex flex-col justify-center h-[360px] group-hover:z-5 transition-all duration-500"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Work email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="e.g: johndoe@gmail.com"
+                        className="h-12 rounded-lg"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      This is a hint text to help user.
+                    </FormDescription>
+                    <p>We will send a text with a verification code.</p>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex items-center gap-x-3">
+                      <FormControl>
+                        <Checkbox {...field} />
+                      </FormControl>
+                      <FormLabel>Remember me</FormLabel>
+                    </div>
+                    <FormDescription>
+                      This is a hint text to help user.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <CustomButton type="submit" className="w-full max-h-12">
+                Submit
+              </CustomButton>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </section>
   );
 }
