937895ea1156242e3e9483cfb42943ef1230a37b
umidjonvokhidov <umidjonvokhidov.uz@gmail.com>
2025-10-16 21:11:49 +0500

    fix in integrations section

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -2,12 +2,16 @@
 
 import CustomButton from '@/components/CustomButton';
 import { INTEGRATION_ICONS } from '@/lib/constants';
+import { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 export default function Integrations() {
+  const [mounted, setMounted] = useState(false);
   const mdScreen = useMediaQuery({ query: '(min-width: 768px)' });
   const lgScreen = useMediaQuery({ query: '(min-width: 1024px)' });
-  const count = lgScreen ? 13 : mdScreen ? 11 : 11;
+
+  useEffect(() => setMounted(true), []);
+  const count = !mounted ? 13 : lgScreen ? 13 : mdScreen ? 11 : 11;
 
   return (
     <section>
@@ -24,6 +28,7 @@ export default function Integrations() {
       </div>
       <div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-10 gap-[1px] p-[1px] bg-white/4 relative overflow-hidden">
         <div className="w-full h-full left-1/2 -translate-x-1/2 -bottom-3/4 absolute opacity-70 bg-gradient-to-b from-blue-500/20 to-white/5 rounded-full blur-[150px]" />
+        <div className="w-full h-full left-1/2 -translate-x-1/2 -top-3/4 absolute opacity-70 bg-gradient-to-b from-blue-500/20 to-white/5 rounded-full blur-[150px]" />
         <div className="col-start-1 row-start-5 col-span-3 row-span-1 md:col-start-2 md:col-span-3 md:row-start-3 md:row-span-2 lg:col-start-4 lg:col-span-4 lg:row-start-2 lg:row-span-1 w-full flex items-center justify-center bg-[#0B0C0C] h-[150px] md:h-full">
           <CustomButton className="w-auto flex-none">
             View All Integrations
