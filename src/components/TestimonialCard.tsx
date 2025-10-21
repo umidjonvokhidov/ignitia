9014e47d38d1d01686ca187188fbc38b84fa731f
umidjonvokhidov <umidjonvokhidov.uz@gmail.com>
2025-10-21 20:41:52 +0500

    testimonial-section done

---
+++ src/components/TestimonialCard.tsx
@@ -0,0 +1,43 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { twMerge } from 'tailwind-merge';
+
+const TestimonialCard = ({
+  Testimonial,
+  className,
+}: {
+  Testimonial: Testimonial;
+  className?: string;
+}) => {
+  const { avatar, company, name, quote, title } = Testimonial;
+
+  const initials = name
+    .split(' ')
+    .map((word) => word[0])
+    .join('')
+    .substring(0, 2)
+    .toUpperCase();
+  return (
+    <div
+      key={name}
+      className={twMerge("grid grid-cols-1 gap-y-6 p-6 rounded-xl border border-white/8 bg-white/4 shadow-[0px_2px_4px_-1px_rgba(14,18,27,0.08)]", className)}
+    >
+      <div className="flex items-center gap-x-3">
+        <Avatar className="size-11">
+          <AvatarImage src={avatar} />
+          <AvatarFallback>{initials}</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col gap-y-0.5 items-start">
+          <h3 className="body-text-small-semi-bold">{name}</h3>
+          <p>
+            {title}, {company}
+          </p>
+        </div>
+      </div>
+      <blockquote className="body-text-large-medium">
+        &quot;{quote}&quot;
+      </blockquote>
+    </div>
+  );
+};
+
+export default TestimonialCard;
--- src/sections/Testimonial.tsx
+++ src/sections/Testimonial.tsx
@@ -1,7 +1,62 @@
+import { CONTACT_SECTIONS, TESTIMONIALS } from '@/lib/constants';
+import TestimonialCard from '@/components/TestimonialCard';
+import Marquee from 'react-fast-marquee';
+import Link from 'next/link';
+
 export default function Testimonial() {
   return (
-    <div>
-      <h3>Testimonial</h3>
-    </div>
+    <section>
+      <div className="flex flex-col items-center px-4 py-6 md:py-8 lg:py-10 max-w-[300px] md:max-w-[350px] lg:max-w-[400px] mx-auto">
+        <h3 className="text-h5 md:text-h4 lg:text-h3 inline-block w-full text-center">
+          What our client <span className="handwrite">says.</span>
+        </h3>
+      </div>
+      <div className="flex flex-col gap-y-4 p-4 visible md:hidden border-y border-white/8">
+        {TESTIMONIALS.map((Testimonial: Testimonial) => (
+          <TestimonialCard key={Testimonial.name} Testimonial={Testimonial} />
+        ))}
+      </div>
+      <div className="w-full overflow-hidden hidden md:block mx-auto relative py-6 md:py-10 border-y border-white/8">
+        <div className="bg-[radial-gradient(ellipse_50%_50%_at_50%_50%,_rgba(12,_12,_12,_0)_0%,_rgba(11,12,12,1)_100%)] mask-alpha absolute inset-0 z-2 pointer-events-none" />
+        <Marquee className="w-full" autoFill speed={25} gradient={false}>
+          <div className="flex gap-x-4 p-2">
+            {TESTIMONIALS.map((Testimonial: Testimonial) => (
+              <TestimonialCard
+                className="max-w-[360px]"
+                key={Testimonial.name}
+                Testimonial={Testimonial}
+              />
+            ))}
+          </div>
+        </Marquee>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        {CONTACT_SECTIONS.map(
+          ({ content, icon: Icon, title, href }: ContactSectionItem) => (
+            <div
+              className="flex flex-col gap-y-3 p-6 md:p-8 lg:p-10 border-b border-white/8 last:border-none md:border-r"
+              key={title}
+            >
+              <div className="flex items-center gap-x-2">
+                <Icon className="text-white" />
+                <span>{title}</span>
+              </div>
+              {!href ? (
+                <p className="body-text-small-regular md:body-text-medium-regular wrap-anywhere">
+                  {content}
+                </p>
+              ) : (
+                <Link
+                  href={href}
+                  className="body-text-small-regular md:body-text-medium-regular text-neutral-500 underline"
+                >
+                  {content}
+                </Link>
+              )}
+            </div>
+          ),
+        )}
+      </div>
+    </section>
   );
 }
