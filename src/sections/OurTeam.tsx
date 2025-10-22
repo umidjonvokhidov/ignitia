4d0a81816febf5ab741c7065810c49ded4f57be3
umidjonvokhidov <umidjonvokhidov.uz@gmail.com>
2025-10-22 19:46:59 +0500

    OurTeam Section done

--- src/sections/OurTeam.tsx
+++ src/sections/OurTeam.tsx
@@ -1,6 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { TEAM_MEMBERS } from '@/lib/constants';
 import Link from 'next/link';
+import FacebookIcon from '@/public/icons/facebook-2.svg';
+import XIcon from '@/public/icons/X.svg';
+import LinkedInIcon from '@/public/icons/linkedin.svg';
+import InstagramIcon from '@/public/icons/instagram.svg';
+import PlusCircleIcon from '@/public/icons/plus-circle.svg';
 
 export default function OurTeam() {
   return (
@@ -10,7 +15,7 @@ export default function OurTeam() {
           Meet the <span className="handwrite">minds</span> behind Ignitia.
         </h3>
       </div>
-      <div className="grid grid-cols-3 gap-[1px] bg-white/4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0.5 border-t border-white/8 bg-white/4">
         {TEAM_MEMBERS.map(
           ({
             avatar,
@@ -26,7 +31,7 @@ export default function OurTeam() {
               .toUpperCase();
             return (
               <div
-                className="flex flex-col gap-y-6 w-full items-center bg-white/4"
+                className="flex flex-col gap-y-6 w-full p-6 items-center bg-white/4"
                 key={title}
               >
                 <Avatar className="size-14">
@@ -34,19 +39,59 @@ export default function OurTeam() {
                   <AvatarFallback>{initials}</AvatarFallback>
                 </Avatar>
                 <div className="flex flex-col items-center gap-y-1">
-                  <h4>{name}</h4>
-                  <h5>{title}</h5>
+                  <h4 className="body-text-large-semi-bold text-center">
+                    {name}
+                  </h4>
+                  <h5 className="body-text-medium-regular text-neutral-500 text-center">
+                    {title}
+                  </h5>
                 </div>
                 <div className="flex items-center gap-x-3">
-                  {instagram && <Link href={instagram} className='rounded-[12px] bg-white/4 p-2.5'></Link>}
-                  {facebook && <Link href={facebook} className='rounded-[12px] bg-white/4 p-2.5'></Link>}
-                  {x && <Link href={x} className='rounded-[12px] bg-white/4 p-2.5'></Link>}
-                  {linkedin && <Link href={linkedin} className='rounded-[12px] bg-white/4 p-2.5'></Link>}
+                  {instagram && (
+                    <Link
+                      href={instagram}
+                      target="_blank"
+                      className="rounded-[12px] bg-white/4 hover:bg-white group p-2.5"
+                    >
+                      <InstagramIcon className="group-hover:text-neutral-500" />
+                    </Link>
+                  )}
+                  {facebook && (
+                    <Link
+                      href={facebook}
+                      target="_blank"
+                      className="rounded-[12px] bg-white/4 p-2.5 hover:bg-white group"
+                    >
+                      <FacebookIcon className="group-hover:text-neutral-500" />
+                    </Link>
+                  )}
+                  {x && (
+                    <Link
+                      href={x}
+                      target="_blank"
+                      className="rounded-[12px] bg-white/4 p-2.5 hover:bg-white group"
+                    >
+                      <XIcon className="group-hover:text-neutral-500" />
+                    </Link>
+                  )}
+                  {linkedin && (
+                    <Link
+                      href={linkedin}
+                      target="_blank"
+                      className="rounded-[12px] bg-white/4 p-2.5 hover:bg-white group"
+                    >
+                      <LinkedInIcon className="group-hover:text-neutral-500" />
+                    </Link>
+                  )}
                 </div>
               </div>
             );
           },
         )}
+        <Link href="#" className="flex flex-col items-center justify-center sm:col-span-2 lg:col-span-1 p-6 gap-x-6 cursor-pointer bg-white/4">
+          <PlusCircleIcon />
+          <h5 className="body-text-large-semi-bold">Join to our team</h5>
+        </Link>
       </div>
     </section>
   );
