cdb4b7894189a4c587b5372b8f0b7b25b3c090c2
umidjonvokhidov <umidjonvokhidov@proton.me>
2025-10-30 19:59:31 +0500

    fixed text in legal pages

--- src/components/LegalHero.tsx
+++ src/components/LegalHero.tsx
@@ -7,7 +7,7 @@ const LegalHero = ({
 }) => {
   return (
     <section className="flex items-center justify-center flex-col gap-y-4 md:gap-y-6 py-20 px-4 md:py-24">
-      <h1 className="text-h5 md:text-h4 lg:text-h3">{title}</h1>
+      <h1 className="text-h5 md:text-h4 lg:text-h3 text-center">{title}</h1>
       <p className="body-text-medium-regular text-white">{updateDate}</p>
     </section>
   );
